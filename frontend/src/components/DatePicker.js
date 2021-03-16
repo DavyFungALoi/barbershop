6cf2f3cea1236fe494ece851dac16613c5ea0989
Davy <davyfungaloi@gmail.com>
2021-03-16 16:38:10 +0100

    changed from array to creating element

--- frontend/src/components/DatePicker.js
+++ frontend/src/components/DatePicker.js
@@ -3,79 +3,57 @@ import { useEffect, useState } from "react";
 import { map } from "../sampleAppointment";
 
 const DatePicker = () => {
-  const [dayArray, setDayArray] = useState([]);
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [currentMonth, setCurrentMonth] = useState([]);
+  const [selectedDate, setSelectedDate] = useState("No Date");
+
+  const currentDate = new Date().toDateString();
+  const populateCalendar = () => {
+    const DayOverview = document.querySelector(".DayOverView");
+    DayOverview.innerHTML = "";
+    createDays();
+  };
   const MonthDuration = 31;
   const createDays = () => {
     for (let i = 0; i < MonthDuration; i++) {
-      dayArray.push(i);
+      const DayOverview = document.querySelector(".DayOverView");
+      const day_element = document.createElement("button");
+      day_element.classList.add("day");
+      day_element.innerHTML = i + 1;
+      DayOverview.appendChild(day_element);
     }
   };
 
-  
   useEffect(() => {
-    createDays();
-    console.log(dayArray);
-  });
+    populateCalendar();
+    console.log(currentDate);
+  }, []);
 
-  return <div>
-       <h1>Pick your Date</h1>
-       <div className="MonthOverview">
-        <div>Month</div>
+  return (
+    <div>
+      <h1>Pick your Date</h1>
+      <div className="MonthOverview">
+        <div>{currentDate}</div>
         <div>Previous Month</div>
         <div>Next Month</div>
       </div>
-      <div className="DayOverView">
-        <div>{dayArray.map((day) => 
-        <div key={day.toString()}>hello</div>
-        )}</div>
-      </div>
-
-  </div>;
+      <div className="DayOverView"></div>
+      <div>You currently have {selectedDate} Selected</div>
+    </div>
+  );
 };
 
 export default DatePicker;
-
-/*
- {slotsArray.map((timeSlot) => (
-        <div key={timeSlot.timeSlotId}>
-          <button
-            onClick={() =>
-              TimeSlotHandler(timeSlot.timeSlotStart, timeSlot.timeSlotEnd)
-            }
-          >
-            {timeSlot.timeSlotStart} - {timeSlot.timeSlotEnd}
-          </button>
-        </div>
-*/
-/*
-
- <h1>Pick your Date</h1>
-      <div className="SelectedDate">Selected Date</div>
-      <div className="MonthOverview">
-        <div>Month</div>
-        <div>Previous Month</div>
-        <div>Next Month</div>
-      </div>
-      <div className="DayOverView">
-        <div>{dayArray.map((day) => 
-        <div key={day.toString()}>hello</div>
-        )}</div>
-      </div>
-      )
-
-
-
- <div>
-      <h1>Pick your Date</h1>
-      <div className="SelectedDate">Selected Date</div>
-      <div className="MonthOverview">
-        <div>Month</div>
-        <div>Previous Month</div>
-        <div>Next Month</div>
-      </div>
-      <div className="DayOverView">
-        <div>{dayArray.map((day) => console.log("hello"))}</div>
-      </div>
-      )
-    </div>
-*/
