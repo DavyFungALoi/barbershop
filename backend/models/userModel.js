c6353576db408f6fe035092928cd618542260244
Davy <davyfungaloi@gmail.com>
2021-03-20 21:10:01 +0100

    added bcrypt & jsonwebtoken for authentication and encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema(
     {
@@ -28,6 +29,23 @@ const userSchema = mongoose.Schema(
     },
     { timestamps: true }
   );
+
+  userSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password)
+  }
+  ///this. allows access to the password in the fields by adding a method to the schema, you can call this without importing it. it compares the entered password and the hashed dashboard.
+
+  userSchema.pre("save", async function (next){
+    if (!this.isModified('password')) {  ///checks if password has been changed, if password hasn't been changed this shouldn't run, it calls next to continue with doing stuff
+      next()
+    }
+  
+    const salt =  await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt)
+  })
+
+  //generates a hashed password
+  
 
   const User = mongoose.model("User", userSchema);
 
