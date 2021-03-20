4edc56f7e7ae1779acb3364c97a8d0061250e6cd
Davy <davyfungaloi@gmail.com>
2021-03-20 21:29:53 +0100

    build an authentication route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,42 +1,76 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 
+//@desc        Register a new User
+//@route       POST /api/users/
+//@access      Public
 
 const registerUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
-    const userExists = await User.findOne({ email });
-  
-    if (userExists) {
-      res.status(400);
-      throw new Error("User already exists");
-    }
-    const user = await User.create({
-      name,
-      email,
-      password,
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
     });
-    if (user) {
-      res.status(201).json({
-        _id: user._id,
-        name: user.name,
-        email: user.email,
-        isAdmin: user.isAdmin
-      });
-    } else {
-      res.status(400);
-      throw new Error("Invalid user data");
-    }
-  });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+});
 
- const getUsers = asyncHandler(async (req, res) => {
-    const users = await User.find({})
-    res.json(users)
-  });
+//@desc        Get an overview of all users
+//@route       GET /api/users/
+//@access      Public
 
-  const getBarbers = asyncHandler(async (req, res) => {
-    const barbers = await User.find({"isBarber": true})
-    res.json(barbers)
-  });
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
 
-  
- export {registerUser, getUsers,getBarbers} 
+//@desc        Get an overview of all users that are barbers
+//@route       GET /api/users/
+//@access      Public
+
+const getBarbers = asyncHandler(async (req, res) => {
+  const barbers = await User.find({ isBarber: true });
+  res.json(barbers);
+});
+
+//@desc        Authenticate a user
+//@route       POST /api/users/login
+//@access      Public
+
+  const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const userExists = await User.findOne({ email, password });
+
+  if (userExists) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("No email or user found");
+  }
+});
+
+export { registerUser, getUsers, getBarbers, authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 const router = express.Router()
-import {registerUser, getUsers, getBarbers}  from "../controllers/userController.js"
+import {registerUser, getUsers, getBarbers, authUser}  from "../controllers/userController.js"
 
 
 
 router.route("/").post(registerUser).get(getUsers);
 router.route("/barbers").get(getBarbers);
+router.route("/login").post(authUser)
 
 
 
