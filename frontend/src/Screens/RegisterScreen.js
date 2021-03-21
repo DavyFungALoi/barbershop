96ea75ce3eb2aae33fac9bdd16bab5971bf2eb78
Davy <davyfungaloi@gmail.com>
2021-03-21 14:11:39 +0100

    added the proxy in order to target the 5000 route

--- frontend/src/Screens/RegisterScreen.js
+++ frontend/src/Screens/RegisterScreen.js
@@ -1,27 +1,64 @@
-import React from 'react'
-import { useDispatch, useSelector} from 'react-redux'
-import { userRegister} from "../actions/userActions"
-
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { userRegister } from "../actions/userActions";
 
 const RegisterScreen = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log('test')
+    dispatch(
+      userRegister(
+        name,
+        email,
+        password,
+      )
+    );
+  };
+  
 
-    return (
-        <div>
-            <div>Please Register</div>
-        <form>
-            <label for='name'>Name</label>
-            <input type = 'text' id='name'></input>
-            <label for='email'>Email</label>
-            <input type = 'text' id='email'></input>
-            <label for='password'>Password</label>
-            <input type = 'text' id='password'></input>
-            <button type='submit'>Register</button>
-        </form>
+  /*const submitHandler = (e) => {
+    e.preventDefault()
+    console.log("test");
+  };
+  */
+
+  return (
+    <div>
+      <div>Please Register</div>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          id="name"
+          value={name}
+        ></input>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          id="email"
+          value={email}
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          type="text"
+          onChange={(e) => setPassword(e.target.value)}
+          id="password"
+          value={password}
+        ></input>
+        <button type="submit">
+          Register
+        </button>
+      </form>
     </div>
-    )
-}
+  );
+};
 
-export default RegisterScreen
+export default RegisterScreen;
