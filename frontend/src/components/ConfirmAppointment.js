9aa52b1de22021a12f98f20830c2053ef52d5304
Davy <davyfungaloi@gmail.com>
2021-03-22 15:54:04 +0100

    made a component for confirming appointment

--- frontend/src/components/ConfirmAppointment.js
+++ frontend/src/components/ConfirmAppointment.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const ConfirmAppointment = () => {
-    return (
-        <div>
-            Your current appointment is
-            <button>Confirm Appointment</button>
-        </div>
-    )
-}
+  const appointmentDetails = useSelector(
+    (state) => state.addAppointmentDetails
+  );
+  const { appointmentDate, timeSlot, barber } = appointmentDetails;
+  return (
+    <div>
+      {`Your current appointment is ${appointmentDate}, ${timeSlot}, ${barber}`} 
+      <button>Confirm Appointment</button>
+    </div>
+  );
+};
 
-export default ConfirmAppointment
+export default ConfirmAppointment;
