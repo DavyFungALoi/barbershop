bb20eb23c4a7d0939c965567db183d6268de23b4
Davy <davyfungaloi@gmail.com>
2021-03-26 22:46:27 +0100

    added the ability to render differently based on succesfull making an appointment

--- frontend/src/components/ConfirmAppointment.js
+++ frontend/src/components/ConfirmAppointment.js
@@ -8,16 +8,20 @@ const ConfirmAppointment = () => {
   const appointmentDetails = useSelector(
     (state) => state.addAppointmentDetails
   );
+  const createAppointmentSelector = useSelector((state) => state.createAppointment);
+  const { success, error } = createAppointmentSelector;
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
   const { appointmentDate, timeSlot, barber } = appointmentDetails;
   const { barberName, barberId } = barber;
+
   const confirmHandler = () => {
     dispatch(
       createAppointment(userInfo._id, barberId, appointmentDate, timeSlot)
     );
   };
+
   return (
     <div>
       {`Your current appointment is ${appointmentDate}, ${timeSlot} with ${barberName}`}
@@ -28,9 +32,21 @@ const ConfirmAppointment = () => {
       >
         Confirm Appointment
       </button>
-      <div>{`${userInfo._id}`}</div>
+      {success ? <div>Your appointment has been succesfully made</div> : <></>}
+      {error ? (
+        <div>
+          That time and date is already taken with that barber, please select a
+          different one.
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
 
 export default ConfirmAppointment;
+
+/*
+
+*/
