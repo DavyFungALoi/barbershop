3ff6efc8f8d61774976d98012eb230d62a5c79ea
Davy <davyfungaloi@gmail.com>
2021-04-11 13:04:41 +0200

    added logout redirect value

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogout } from "../actions/userActions";
+import { useHistory } from "react-router-dom";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
-  const dispatch = useDispatch();
+  const { logOutSuccess } = userLogin;
 
   const logoutHandler = () => {
     dispatch(userLogout());
   };
+
+  useEffect(() => {
+    if (logOutSuccess) {
+      history.push(`/`);
+    }
+  }, [history, logOutSuccess]);
 
   return (
     <div>
@@ -19,7 +29,7 @@ const Header = () => {
       <div>Location</div>
       <div>Make Appointment</div>
       {userInfo ? (
-        <a href="users/profile">{userInfo.name}</a>
+        <a href="/users/profile">{userInfo.name}</a>
       ) : (
         <a href="/login">
           <button>Login Now</button>
--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -30,7 +30,7 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
-      return {};
+      return { logOutSuccess: true };
     default:
       return state;
   }
